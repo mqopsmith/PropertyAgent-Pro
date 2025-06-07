f6476cc96908c21ca0a24a8448fc25f07afb4f27
mqopsmith <mingquan@opsmith.biz>
2025-06-07 23:10:53 +0800

    Fix Next.js config to enable API routes (remove static export)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Remove static export to enable API routes
   trailingSlash: true,
-  distDir: 'out'
 }
 
 module.exports = nextConfig
